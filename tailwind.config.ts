012a296602712a10f0cf30c782dcdaa3b86a481a
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-11-13 23:28:17 +0700

    feat: config tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                Red: '#db2d2e',
+                White: '#ffffff',
+                Black: '#000000',
+                Orange: '#db2d2e',
+                WhiteGray: '#bfa37c',
+                Blur: '#f7f5f2'
+            }
+        },
+        screens: {
+            xs: '480px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px'
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 };
 export default config;
